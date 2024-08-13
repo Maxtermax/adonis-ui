741b74e740bc11dc9e655c0c5835384ad46c67d8
maxtermax <esnene02@gmail.com>
2024-08-12 23:58:07 -0500

    Adding theme HOC

--- src/core/components/ADMedia/components/Thumbnails/styles.js
+++ src/core/components/ADMedia/components/Thumbnails/styles.js
@@ -1,8 +1,9 @@
+import { withTheme } from '@emotion/react';
 import styled from "@emotion/styled";
 
-export const Image = styled.img`
+export const Image = withTheme(styled.img`
   object-fit: contain;
   background-color: ${({ theme }) => theme.colors.white};
   opacity: ${({ theme, selected = false }) =>
     selected ? theme.opacity.visible : theme.opacity.middle} !important;
-`;
+`);
