9973f1fdf168f4d5b9535bef725103d784faa9e3
maxtermax <esnene02@gmail.com>
2024-09-24 07:43:33 -0500

    Debuging focus

--- src/core/components/ADLayout/components/ADLayoutOption/ADLayoutOption.jsx
+++ src/core/components/ADLayout/components/ADLayoutOption/ADLayoutOption.jsx
@@ -7,32 +7,40 @@ import * as mutations from "ADLayout/mutations";
 import * as styles from "./styles";
 
 export const ADLayoutOption = (props = {}) => {
-  const handleFocusChange = (value, _resolver, setNoUpdate, currentState) => {
-    const match = value?.focus === props.id || currentState?.focus === props.id;
-    // setNoUpdate(!match);
-    console.log({ focus: value.focus, match, id: props.id });
-    return { focus: value?.focus ?? "" };
-  };
-
-  const { state } = useMutations({
+  const { state, onEvent } = useMutations({
     initialState: { focus: "" },
     noUpdate: false,
-    events: [actions.FOCUS_OPTION, actions.BLUR_OPTION],
-    onChange: handleFocusChange,
     store: layoutMicroStore,
     id: LAYOUT_HEADER_STORE,
   });
 
+  const handleFocus = (value, _, setNoUpdate) => {
+    const hasNotMatch = value?.focus !== props.id;
+    setNoUpdate(hasNotMatch);
+    return { focus: value?.focus ?? "" };
+  };
+
+  const handleBlur = (value, __, setNoUpdate) => {
+    const hasNotMatch = value?.focus !== props.id;
+    setNoUpdate(hasNotMatch);
+    return { focus: "" };
+  };
+
+  onEvent(actions.FOCUS_OPTION, handleFocus);
+  // onEvent(actions.BLUR_OPTION, handleBlur);
+
   const isSelected = state.focus === props.id;
 
-  const handleEnter = (focus = "") => {
+  const handleEnter = () => {
+    const { id: focus } = props;
+    console.trace("focus:" + focus);
     const store = layoutMicroStore.get(LAYOUT_HEADER_STORE);
     mutations.focusOption(store, { focus }, [LAYOUT_HEADER_STORE]);
   };
 
   return (
     <styles.Item isSelected={isSelected}>
-      <ADButton onMouseEnter={() => handleEnter(props.id)} variant="text">
+      <ADButton onMouseEnter={handleEnter} variant="text">
         <ADText value={props.name} variant="subtitle" />
       </ADButton>
     </styles.Item>
--- src/core/components/ADLayout/store/layout.js
+++ src/core/components/ADLayout/store/layout.js
@@ -3,4 +3,6 @@ import { MicroStore } from "hermes-io";
 export const LAYOUT_HEADER_STORE = 'LAYOUT_STORE';
 
 export const layoutMicroStore = new MicroStore();
+window.layoutMicroStore = layoutMicroStore;
+
 
